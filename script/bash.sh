1f0d50298607a31c60720275b81a8b8776abe95a
temi111 <temilab@outlook.com>
2025-05-18 16:00:05 +0100

    added first code

---
+++ script/bash.sh
@@ -0,0 +1,23 @@
+echo "What is your name?"
+read name
+echo "Hello $name"
+
+echo "What is your last name?"
+read last_name
+echo "Hello $name $last_name"
+
+echo "What is your age?"
+read age
+echo "Hello $name $last_name, you are $age years old"
+
+echo "What is your favorite color?"
+read color
+echo "Hello $name $last_name, you are $age years old and your favorite color is $color"
+
+echo "What is your favorite food?"
+read food
+echo "Hello $name $last_name, you are $age years old, your favorite color is $color and your favorite food is $food"
+
+echo "What is your favorite movie?"
+read movie
+echo "Hello $name $last_name, you are $age years old, your favorite color is $color, your favorite food is $food and your favorite movie is $movie"
