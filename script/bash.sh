9e60f3c51757fa24cb7516dae13f4c80bb72e207
temi111 <temilab@outlook.com>
2025-05-18 16:23:44 +0100

    added first code

--- script/bash.sh
+++ script/bash.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Exit on any error
-set -e
+# Use default values if environment variables are not set
+USER_NAME="${USER_NAME:-John}"
+LAST_NAME="${LAST_NAME:-Doe}"
+AGE="${AGE:-30}"
+FAVORITE_COLOR="${FAVORITE_COLOR:-blue}"
+FAVORITE_FOOD="${FAVORITE_FOOD:-pizza}"
+FAVORITE_MOVIE="${FAVORITE_MOVIE:-Inception}"
 
 echo "Hello $USER_NAME"
-
 echo "Hello $USER_NAME $LAST_NAME"
-
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old"
-
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old and your favorite color is $FAVORITE_COLOR"
-
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR and your favorite food is $FAVORITE_FOOD"
-
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR, your favorite food is $FAVORITE_FOOD and your favorite movie is $FAVORITE_MOVIE"
