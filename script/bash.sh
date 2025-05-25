cdf70fdaeca9c9a8498117d4e8f74f80983d8f9d
Temitope <46519985+Temi111@users.noreply.github.com>
2025-05-25 15:01:34 +0100

    Update bash.sh

--- script/bash.sh
+++ script/bash.sh
@@ -14,3 +14,4 @@ echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old"
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old and your favorite color is $FAVORITE_COLOR"
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR and your favorite food is $FAVORITE_FOOD"
 echo "Hello $USER_NAME $LAST_NAME, you are $AGE years old, your favorite color is $FAVORITE_COLOR, your favorite food is $FAVORITE_FOOD and your favorite movie is $FAVORITE_MOVIE"
+# to test git merge
