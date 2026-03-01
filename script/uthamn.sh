075e361544d12a2f518fc7d659b98fd908ee217d
temi111 <temilab@outlook.com>
2026-03-01 14:34:14 +0000

    added

---
+++ script/uthamn.sh
@@ -0,0 +1,16 @@
+# pwd                 # Show current directory
+# ls -la              # List all files including hidden
+# cd folder-name      # Change directory
+# mkdir new-folder    # Create directory
+# rm -rf folder       # Remove directory (force)
+# cp file1 file2      # Copy file
+# mv old new          # Move or rename file
+
+
+Name="uthman"
+LOCATION="Ireland"
+AGE="33"
+
+echo "Name is: $Name"
+echo "Location: $LOCATION"
+echo "Age: $AGE"
